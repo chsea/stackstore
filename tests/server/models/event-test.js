2f117f8602dd15b871856eba249d0ea093a515a5
Cristina <cristina.colon+git@gmail.com>
2015-09-14 18:53:41 -0400

    started tests for event model

---
+++ tests/server/models/event-test.js
@@ -0,0 +1,11 @@
+var dbURI = 'mongodb://localhost:27017/testingDB';
+var clearDB = require('mocha-mongoose')(dbURI);
+
+var sinon = require('sinon');
+var expect = require('chai').expect;
+var mongoose = require('mongoose');
+
+// Require in all models.
+require('../../../server/db/models');
+
+var eventProduct = mongoose.model('eventProduct');
