793dd5f3d727833fc43a0cdd284728fe62020ff7
Cristina <cristina.colon+git@gmail.com>
2015-09-16 15:12:49 -0400

    now using JSData for venue state

--- browser/js/venue/venue.factory.js
+++ browser/js/venue/venue.factory.js
@@ -1,10 +1,25 @@
-app.factory('VenueFactory', function ($http){
-	function getOne(id) {
-		return $http.get('/api/venues/'+id)
-			.then(function(resp){return resp.data; });
-	}
+// app.factory('VenueFactory', function ($http){
+// 	function getOne(id) {
+// 		return $http.get('/api/venues/'+id)
+// 			.then(function(resp){return resp.data; });
+// 	}
 
-	return {
-		getOne: getOne
-	};
-});
+// 	return {
+// 		getOne: getOne
+// 	};
+// });
+
+// JS Data version
+app.factory('Venue', function(DS, $state) {
+	return DS.defineResource({
+		name: 'venues',
+		methods: {
+			go: function() {
+				$state.go('venue', {
+					venueId: this._id
+				});
+			}
+		}
+
+	});
+}).run(function (Venue) {});
--- browser/js/venue/venue.js
+++ browser/js/venue/venue.js
@@ -1,10 +1,25 @@
-app.config(function ($stateProvider) {
-    $stateProvider.state('venue', {
-        url: '/venue/:id',
-        templateUrl: 'js/venue/venue.html',
-        controller: 'VenueCtrl',
-        resolve: {
-        	venue: function ($stateParams, VenueFactory) { return VenueFactory.getOne($stateParams.id); }
-        }
-    });
+// app.config(function ($stateProvider) {
+//     $stateProvider.state('venue', {
+//         url: '/venue/:id',
+//         templateUrl: 'js/venue/venue.html',
+//         controller: 'VenueCtrl',
+//         resolve: {
+//         	venue: function ($stateParams, VenueFactory) { return VenueFactory.getOne($stateParams.id); }
+//         }
+//     });
+// });
+
+// JSData version
+app.config(function($stateProvider) {
+	$stateProvider.state('venue', {
+		url: '/venue/:id',
+		templateUrl: 'js/venue/venue.html',
+		controller: 'VenueCtrl',
+		resolve: {
+			venue: function($stateParams, Venue){
+				// GET --> /api/venues
+				return Venue.find($stateParams.id);
+			}
+		}
+	});
 });
