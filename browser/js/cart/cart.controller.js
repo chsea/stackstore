f2224f1a715eabc6cf90602da114b60674174ae2
Daniel Perrelly <danperrelly@gmail.com>
2015-09-16 17:58:40 -0400

    removed console log

--- browser/js/cart/cart.controller.js
+++ browser/js/cart/cart.controller.js
@@ -1,7 +1,6 @@
 app.controller('cartCtrl',function($scope, $state, CartFactory, cart){
 	$scope.cart = cart;
 	$scope.removeTicket = function(ticketId){
-			console.log('hi');
 		CartFactory.delete(ticketId).then(function(newCart){
 			$scope.cart = newCart;
 		});
