5f3a667f304a787e57d253d4095f78f4f6cfa861
Chelsea Du <chsea.du@gmail.com>
2015-09-17 15:51:12 -0400

    fix event controller

--- browser/js/event/event.js
+++ browser/js/event/event.js
@@ -7,7 +7,10 @@ app.config(function($stateProvider) {
 			eventData: function($stateParams, Event){
 				// GET --> /api/events
 				return Event.find($stateParams.id);
-			}
+			},
+			user: function(User, AuthService){
+        return AuthService.getLoggedInUser();
+      }
 		}
 	});
 	$stateProvider.state('events', {
