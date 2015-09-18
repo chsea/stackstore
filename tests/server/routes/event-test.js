fd84a820d9072d27d0e2710012e7251784200723
Daniel Moennich <daniel.moennich@gmail.com>
2015-09-17 20:20:45 -0400

    Event route tests done.

--- tests/server/routes/event-test.js
+++ tests/server/routes/event-test.js
@@ -1,7 +1,10 @@
 // Instantiate all models
 var mongoose = require('mongoose');
 require('../../../server/db/models');
-var EventProduct = mongoose.model('EventProduct');
+var Event = mongoose.model('Event');
+var EventType = mongoose.model('EventType');
+var Venue = mongoose.model('Venue');
+var Promise = require("bluebird");
 
 var expect = require('chai').expect;
 
@@ -12,6 +15,15 @@ var supertest = require('supertest');
 var app = require('../../../server/app');
 
 describe('Events Route', function () {
+
+  before(function () {
+    process.env.TEST = true;
+  });
+
+  before(function () {
+    process.env.TEST = false;
+  });
+
 	beforeEach('Establish DB connection', function (done) {
 		if (mongoose.connection.db) return done();
 		mongoose.connect(dbURI, done);
@@ -21,83 +33,134 @@ describe('Events Route', function () {
 		clearDB(done);
 	});
 
-	var guestAgent, createdEvent;
+	var guestAgent;
 
 	beforeEach('Create guest agent', function () {
 		guestAgent = supertest.agent(app);
 	});
 
-	beforeEach('Create an event', function (done) {
-		EventProduct
-    .create([{name: 'BSB at MSG', date: new Date()}, {name: 'LOL at MSG', date: new Date()}])
-    .then(function(events) {
-      createdEvent = events[0];
-      done();
-    }).then(null, done);
-	});
+
+  var createEvents = function (count) {
+    var promises = [];
+    while (count > 0) {
+      promises.push(createEvent());
+      count -= 1;
+    }
+    return Promise.all(promises);
+  };
+
+
+  var createEvent = function () {
+    var eventType;
+    return EventType.create({
+            name: 'Backstreet Boys'
+        })
+        .then(function(newEventType) {
+            eventType = newEventType;
+            return Venue.create({
+                name: 'Madison Square Garden',
+                address: {
+                  streetAddress: '4 Pennsylvania Plaza',
+                  city: 'New York',
+                  state: 'NY',
+                  zip: 10001
+                },
+                coordinates: [40.7505045, -73.9934387],
+                seatingMapUrl: '/images/madisonSqGardenSeatMap.png'
+            });
+        })
+        .then(function (newVenue) {
+          return Event.create({
+            EventType: eventType,
+            Venue: newVenue,
+            date: new Date()
+          });
+        });
+  };
 
 	describe('Get request', function () {
 		it('should get all events', function (done) {
-			guestAgent.get('/api/events')
-      .expect(200)
-      .end(function(err, res){
-        if(err) done(err);
-        expect(res.body.length).to.equal(2);
-        done();
+      createEvents(2)
+      .then(function (events) {
+        guestAgent.get('/api/events')
+        .expect(200)
+        .end(function(err, res){
+          if(err) done(err);
+          expect(res.body.length).to.equal(2);
+          done();
+        });
       });
 		});
 
 		it('should get an event by id', function (done) {
-			guestAgent.get('/api/events/' + createdEvent._id)
-				.end(function(err, res){
-					if(err) done(err);
-					expect(res.body.name).to.equal('BSB at MSG');
-					done();
-				});
+      createEvent()
+      .then(function (createdEvent) {
+        guestAgent.get('/api/events/' + createdEvent._id)
+        .end(function(err, res){
+            if(err) done(err);
+            expect(res.body._id).to.equal(createdEvent._id.toString());
+            done();
+        });
+      });
 		});
+
 	});
 
 	it('should create a new event', function (done) {
-		guestAgent.post('/api/events')
-    .send({name: '3EB at MSG', date: new Date()})
-    .expect(201)
-    .end(function(err, res) {
-      guestAgent.get('/api/events')
-      .expect(200)
-      .end(function(err, res){
-        if(err) done(err);
-        expect(res.body.length).to.equal(3);
-        done();
+    createEvent()
+    .then(function (createdEvent) {
+      guestAgent.post('/api/events')
+      .send({Venue: createdEvent.Venue._id, EventType: createdEvent.EventType._id, date: new Date()})
+      .expect(201)
+      .end(function(err, res) {
+        guestAgent.get('/api/events')
+        .expect(200)
+        .end(function(err, res){
+          if(err) done(err);
+          expect(res.body.length).to.equal(2);
+          done();
+        });
       });
     });
+
 	});
 
   it('should update an event', function (done) {
-		guestAgent.put('/api/events/' + createdEvent._id)
-    .send({name: '3EB at MSG'})
-    .expect(200)
-    .end(function(err, res) {
-      guestAgent.get('/api/events/' + createdEvent._id)
+    createEvent()
+    .then(function (createdEvent) {
+      var date = new Date(1984, 5, 19);
+      guestAgent.put('/api/events/' + createdEvent._id)
+      .send({date: date})
       .expect(200)
-      .end(function(err, res){
-        if(err) done(err);
-        expect(res.body.name).to.equal('3EB at MSG');
-        done();
+      .end(function(err, res) {
+        guestAgent.get('/api/events/' + createdEvent._id)
+        .expect(200)
+        .end(function(err, res){
+          if(err) done(err);
+          expect(res.body.date).to.equal(date.toISOString());
+          done();
+        });
       });
     });
-	});
+
+  });
 
   it('should delete an event', function (done) {
-		guestAgent.delete('/api/events/' + createdEvent._id)
-    .expect(204)
-    .end(function(err, res) {
-      guestAgent.get('/api/events')
-      .expect(200)
-      .end(function(err, res){
-        if(err) done(err);
-        expect(res.body.length).to.equal(1);
-        done();
+
+    createEvent().
+    then(function (createdEvent) {
+  		guestAgent.delete('/api/events/' + createdEvent._id)
+      .expect(204)
+      .end(function(err, res) {
+        guestAgent.get('/api/events')
+        .expect(200)
+        .end(function(err, res){
+          if(err) done(err);
+          expect(res.body.length).to.equal(0);
+          done();
+        });
       });
     });
-	});
+
+  });
 });
