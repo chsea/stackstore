4340ab262ccc1570ff6a5d6c4625fa195105dac8
Daniel Moennich <daniel.moennich@gmail.com>
2015-09-18 19:48:16 -0400

    Removed console.log

--- browser/js/moreDates/moreDates.controller.js
+++ browser/js/moreDates/moreDates.controller.js
@@ -1,5 +1,4 @@
 app.controller('MoreDatesCtrl',function($scope, $state, $stateParams, TicketsForEventFactory){
-	console.log("scope:", $scope);
 	TicketsForEventFactory.getAlternateDates($scope.eventData.EventType._id).then(function(dates){
 		$scope.dates = dates;
 	});
