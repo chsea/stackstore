df4be9be70d34f41248735abc0460c3d9a096f87
Cristina <cristina.colon+git@gmail.com>
2015-09-20 15:42:45 -0400

    add option to filter by category

--- browser/js/event/event.controller.js
+++ browser/js/event/event.controller.js
@@ -1,11 +1,16 @@
-// app.controller('EventCtrl',function($scope, $state, EventFactory, eventData){
-// 	$scope.eventData = eventData;
-// });
-
-// JS Data version
-app.controller('EventCtrl', function($scope, $mdBottomSheet, eventData, user) {
+app.controller('EventCtrl', function ($scope, $mdBottomSheet, Event, eventData, user) {
 	$scope.loggedIn = Boolean(user);
     $scope.eventData = eventData;
+    $scope.refreshEventData = function (category) {
+    	Event.findAll()
+    		.then(function(newData){
+    			if (!category) return newData; 
+    			return newData.filter(e => e.EventType.category==category);
+    		})
+    		.then(function(filtered){
+    			$scope.eventData = filtered;
+    		});
+    }
     $scope.openBottomSheet = function() {
         $mdBottomSheet.show({
             template: '<md-bottom-sheet >Hello!</md-bottom-sheet>'
