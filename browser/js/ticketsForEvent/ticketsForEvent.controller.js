3cf3ed9c2eb37b3925bbbb20c13a0e0e030c2694
Daniel Perrelly <danperrelly@gmail.com>
2015-09-22 15:34:21 -0400

    can remove from cart from event

--- browser/js/ticketsForEvent/ticketsForEvent.controller.js
+++ browser/js/ticketsForEvent/ticketsForEvent.controller.js
@@ -1,8 +1,17 @@
-app.controller('TicketsForEventCtrl',function ($scope, $state, $stateParams, TicketsForEventFactory, CartFactory){
+app.controller('TicketsForEventCtrl',function ($scope, CartFactory){
 	$scope.addToCart = function(id){
 		CartFactory.add(id).finally(function(){
 			$scope.tickets.forEach(function(ticket){
 				if(ticket._id.toString() === id.toString()) ticket.added = true;
+			});
+		});
+	};
+
+	$scope.removeTicket = function(ticketId){
+		console.log(ticketId);
+		CartFactory.delete(ticketId).then(function(newCart){
+			$scope.tickets.forEach(function(ticket){
+				if(ticket._id.toString() === ticketId.toString()) ticket.added = false;
 			});
 		});
 	};
