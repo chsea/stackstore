b3cca1e52d69de6968f17bcb129fcd08c4da7062
Cristina <cristina.colon+git@gmail.com>
2015-09-21 22:46:44 -0400

    aria labels for cart icons

--- browser/js/moreDates/moreDates.controller.js
+++ browser/js/moreDates/moreDates.controller.js
@@ -1,7 +1,6 @@
 app.controller('MoreDatesCtrl',function($scope, $state, $stateParams, TicketsForEventFactory){
 	TicketsForEventFactory.getAlternateDates($scope.eventData.EventType._id).then(function(dates){
 		$scope.currentDate = $scope.eventData.date;
-		console.log(dates);
 		$scope.dates = dates;
 	});
 });
