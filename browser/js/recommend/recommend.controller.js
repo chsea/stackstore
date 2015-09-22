d9b4e5784e1d461d08b7f689859bdd4ecee994f5
Cristina <cristina.colon+git@gmail.com>
2015-09-22 12:16:19 -0400

    workaround for a bug that sometimes prevented recommendations from loading

--- browser/js/recommend/recommend.controller.js
+++ browser/js/recommend/recommend.controller.js
@@ -1,5 +1,5 @@
-app.controller('RecommendCtrl',function($scope, $state, $stateParams, Recommend){
-	Recommend.find($stateParams.id).then(function(recs){
+app.controller('RecommendCtrl',function($scope, $state, $stateParams, TicketsForEventFactory){
+	TicketsForEventFactory.getRecs($stateParams.id).then(function(recs){
 		$scope.recs = recs;
 	});
 });
--- browser/js/ticketsForEvent/ticketsForEvent.factory.js
+++ browser/js/ticketsForEvent/ticketsForEvent.factory.js
@@ -6,8 +6,12 @@ app.factory('TicketsForEventFactory', function ($http){
 	function getAlternateDates(eventTypeId) {
 		return $http.get('/api/eventtypes/'+eventTypeId+'/dates').then(function(resp){return resp.data.filter(e => !e.inactive && new Date() < new Date(e.date)); });
 	}
+	function getRecs (id) {
+		return $http.get('/api/recommend/'+id).then(function(resp){return resp.data; });
+	}
 	return {
 		getAll: getAll,
-		getAlternateDates: getAlternateDates
+		getAlternateDates: getAlternateDates,
+		getRecs: getRecs
 	};
 });
